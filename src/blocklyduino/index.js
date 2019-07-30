ee7d19be79fbe4262758aaa6628081058b4bc3d6
Filip Budiša <filip.budisa@gmail.com>
2019-07-30 13:38:18 +0200

    Moved blocklyduino

---
+++ src/blocklyduino/index.js
@@ -0,0 +1,31 @@
+
+import "js/arduino_boards";
+
+import "blocks/colors";
+import "blocks/arduino_base/arduino_base";
+import "blocks/grove/grove";
+import "blocks/servo/servo";
+
+import "generators/arduino"
+import "generators/arduino/blockly/colour";
+import "generators/arduino/blockly/lists";
+import "generators/arduino/blockly/logic";
+import "generators/arduino/blockly/loops";
+import "generators/arduino/blockly/math";
+import "generators/arduino/blockly/procedures";
+import "generators/arduino/blockly/text";
+import "generators/arduino/blockly/variables";
+import "generators/arduino/blockly/variables_dynamic";
+
+import "generators/arduino/arduino_base";
+import "generators/arduino/addon/grove";
+import "generators/arduino/addon/servo";
+
+import "themes/bw";
+import "themes/classic";
+import "themes/highcontrast";
+import "themes/modern";
+
+import "msg/blocklyduino/en";
+import "msg/blocks/en"
+
