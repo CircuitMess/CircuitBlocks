b6add526201e00628dab7408adc9dcbb4deafad3
Filip Budiša <filip.budisa@gmail.com>
2019-08-17 19:42:06 +0200

    Fixed procedures typo

--- arduino/blocks/procedures.js
+++ arduino/blocks/procedures.js
@@ -31,7 +31,7 @@ Blockly.Blocks['arduino_functions'] = {
         .appendField(Blockly.Msg.ARD_FUN_RUN_LOOP);
     this.appendStatementInput('LOOP_FUNC');
     this.setInputsInline(false);
-    this.setColour(Blockly.Blocks.procedures.HUE);
+    this.setColour(Blockly.Blocks.Procedures.HUE);
     this.setTooltip(Blockly.Msg.ARD_FUN_RUN_TIP);
     this.setHelpUrl('https://arduino.cc/en/Reference/Loop');
     this.contextMenu = false;
