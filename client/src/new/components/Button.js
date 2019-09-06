1bc3103c54d85670bc0505349a5650d09c4b1d07
Fran Zekan <zekan.fran369@gmail.com>
2019-09-06 14:20:26 +0200

    Add button component

---
+++ client/src/new/components/Button.js
@@ -0,0 +1,85 @@
+import styled from 'styled-components';
+
+const Button = styled.div`
+  display: inline-block;
+  border-radius: 8px;
+  background: rgba(255, 255, 255, 0.25);
+  box-shadow: 0px 1px 2px rgba(0, 0, 0, 0.25);
+  margin-right: 12px;
+  cursor: pointer;
+  padding: 6px;
+  transition-duration: 0.3s;
+
+  &.long {
+    width: 100%;
+    box-sizing: border-box;
+  }
+  &.mid {
+    min-width: 50%;
+  }
+
+  &:hover {
+    opacity: 0.8;
+    transition-duration: 0.1s;
+  }
+  &:active,
+  &.selected {
+    opacity: 0.5;
+    transition-duration: 0.1s;
+  }
+
+  &:active:not(.disabled) {
+    transform: translateY(2px);
+  }
+
+  &.disabled {
+    opacity: 0.25;
+  }
+
+  &:last-child {
+    margin-right: 0px !important;
+  }
+
+  i.material-icons,
+  .text {
+    display: inline-block;
+    line-height: 24px;
+    vertical-align: middle;
+  }
+
+  .text {
+    font-style: normal;
+    font-weight: 600;
+    font-size: 10px;
+    height: 24px;
+    margin-right: 4px;
+    margin-left: 4px;
+  }
+
+  ${(props) => props.color && colors[props.color]}
+`;
+
+const colors = {
+  red: {
+    background: '#E3384D',
+    color: 'white'
+  },
+  blue: {
+    background: '#1045BA',
+    color: 'white'
+  },
+  teal: {
+    background: '#00BED6',
+    color: 'white'
+  },
+  white: {
+    background: '#ffffff',
+    color: '#595959'
+  },
+  noFill: {
+    backgroundColor: ' none',
+    boxShadow: 'none'
+  }
+};
+
+export default Button;
