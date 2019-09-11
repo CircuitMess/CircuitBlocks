daf58b088425198c2d291f5b0d4a45645176283d
Filip Budiša <filip.budisa@gmail.com>
2019-09-11 22:15:29 +0200

    Added support for adding code to the loop function. Added mp.update() to the loop

--- client/src/blockly/arduino/generators/makerphone.js
+++ client/src/blockly/arduino/generators/makerphone.js
@@ -6,4 +6,6 @@ Blockly.Arduino.additionals["MAKERphone"] = function(){
   Blockly.Arduino.addInclude("MP_include", "#include <MAKERphone.h>");
   Blockly.Arduino.addDeclaration("MP_declare", "MAKERphone mp;");
   Blockly.Arduino.addSetup("MP_begin", "mp.begin(1);");
+  Blockly.Arduino.addSetup("display_clear", "mp.display.fillScreen(TFT_BLACK);");
+  Blockly.Arduino.addWrap("MP_update", "mp.update();");
 };
