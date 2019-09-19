2aa62b747df6d4e1277c1b03b4ee83ea263db6b3
Fran Zekan <zekan.fran369@gmail.com>
2019-09-19 16:07:39 +0200

    Fix side by side editor

--- client/src/components/BlocklyEditor.js
+++ client/src/components/BlocklyEditor.js
@@ -8,18 +8,19 @@ class BlocklyEditor extends React.Component {
     isCodeOpen: PropTypes.bool.isRequired
   };
 
+  componentDidUpdate() {
+    if (window.Blockly) {
+      window.Blockly.svgResize(this.props.ws);
+    }
+  }
+
   render() {
     const { setRef, height, width, isCodeOpen } = this.props;
     // const [appState, appDispatch] = useAppStateValue();
     // const { isCodeOpen } = appState;
 
     return (
-      <div
-        id="blocklyDiv"
-        className={isCodeOpen ? 'd-none' : ''}
-        style={{ height, width }}
-        ref={setRef}
-      />
+      <div id="blocklyDiv" style={{ height, width: isCodeOpen ? width / 2 : width }} ref={setRef} />
     );
   }
 }
