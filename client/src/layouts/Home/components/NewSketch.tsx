15ef73fa8a7d1d8e576952ce4515abfe0da27a8c
Filip Budiša <filip@circuitmess.com>
2020-08-05 16:03:14 +0200

    Clean up new sketch modal

--- client/src/layouts/Home/components/NewSketch.tsx
+++ client/src/layouts/Home/components/NewSketch.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import {Button, Dimmer} from "semantic-ui-react";
 import {ModalBase} from "../../../components/Modal/Common";
 import {SketchType} from "../../Editor";
-import ReactSwitch from "react-switch";
 
 export interface NewSketchProps {
     open: boolean;
@@ -11,6 +10,7 @@ export interface NewSketchProps {
 
 interface NewSketchState {
     nibble: boolean;
+    type: SketchType;
 }
 
 export class NewSketch extends React.Component<NewSketchProps, NewSketchState> {
@@ -19,34 +19,37 @@ export class NewSketch extends React.Component<NewSketchProps, NewSketchState> {
         super(props);
 
         this.state = {
-            nibble: false
+            nibble: false,
+            type: SketchType.BLOCK
         };
     }
 
-    private create(type: SketchType){
+    private create(){
         const { callback } = this.props;
+        const { type } = this.state;
         callback(type, this.state.nibble ? "cm:esp8266:nibble" : "cm:esp32:ringo");
     }
 
     public render(){
         const { open, callback } = this.props;
-        const { nibble } = this.state;
+        const { nibble, type } = this.state;
 
         return <Dimmer active={open}>
             <ModalBase className={"small"}>
-                <div className="title" style={{ position: "relative", fontSize: 24, top: 0, textAlign: "center", marginBottom: 10, lineHeight: 1.2 }}>New sketch</div>
+                <div className="title" style={{ position: "relative", fontSize: 26, top: 0, textAlign: "center", marginBottom: 30, lineHeight: 1.2 }}>New sketch</div>
                 <div className="content" style={{ display: "flex", flexDirection: "column", justifyContent: "space-between" }}>
-                    <div style={{ display: "flex", flexDirection: "row", justifyContent: "center", alignItems: "center", paddingTop: "10px", fontWeight: "bold" }}>
-                        <span>Ringo</span>
-                        <div style={{ margin: "0 20px" }}>
-                            <ReactSwitch checkedIcon={false} uncheckedIcon={false} offColor={"#E3384D"} onColor={"#FFC629"} checked={ nibble } size={30} onChange={(checked) => { this.setState({ nibble: checked }) }} />
-                        </div>
-                        <span>Nibble</span>
+                    <p style={{ marginBottom: 15, fontSize: 18, fontWeight: "bold" }}>Device:</p>
+                    <div style={{ display: "flex", flexDirection: "row", justifyContent: "center", marginBottom: 20 }}>
+                        <Button primary inverted={nibble} onClick={() => this.setState({ nibble: false }) } style={{margin: "0 15px"}}>Ringo</Button>
+                        <Button primary inverted={!nibble} onClick={() => this.setState({ nibble: true }) } style={{margin: "0 15px"}}>Nibble</Button>
                     </div>
-                    <div style={{ display: "flex", flexDirection: "row", justifyContent: "space-around" }}>
-                        <Button onClick={ () => this.create(SketchType.CODE) } primary style={{ margin: "0 auto", marginTop: 20 }}>Code</Button>
-                        <Button onClick={ () => this.create(SketchType.BLOCK) } primary style={{ margin: "0 auto", marginTop: 20 }}>Block</Button>
+                    <p style={{ marginBottom: 15, fontSize: 18, fontWeight: "bold" }}>Sketch type:</p>
+                    <div style={{ display: "flex", flexDirection: "row", justifyContent: "center" }}>
+                        <Button primary inverted={type != SketchType.CODE} onClick={() => this.setState({ type: SketchType.CODE }) } style={{margin: "0 15px"}}>Code</Button>
+                        <Button primary inverted={type != SketchType.BLOCK} onClick={() => this.setState({ type: SketchType.BLOCK }) } style={{margin: "0 15px"}}>Block</Button>
                     </div>
+
+                    <Button onClick={() => this.create() } color="red" style={{ margin: "0 auto", marginTop: 30, backgroundColor:"#E3384D", width: "50%" }} size={"big"}>Create</Button>
                 </div>
             </ModalBase>
         </Dimmer>
