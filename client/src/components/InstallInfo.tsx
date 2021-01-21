dd1c03e761b35dbd405ccdaea8876d7528179707
Filip Budiša <filip@circuitmess.com>
2021-01-21 15:41:27 +0100

    Remove some jokes, increase cycle duration

--- client/src/components/InstallInfo.tsx
+++ client/src/components/InstallInfo.tsx
@@ -45,7 +45,7 @@ export class InstallInfo extends React.Component<InstallInfoProps, InstallInfoSt
         setInterval(() => {
             if(this.state.stage == "DONE") return;
             this.setState({ jokeIndex: (this.state.jokeIndex+1) % this.Jokes.length });
-        }, 10000);
+        }, 15000);
     }
 
     private retry(){
@@ -145,7 +145,6 @@ export class InstallInfo extends React.Component<InstallInfoProps, InstallInfoSt
         return array;
     })([
         "How did the programmer die in the shower? He read the shampoo bottle instructions. Lather, Rinse, Repeat!",
-        "I will be giving away empty batteries later today. Free of charge!",
         "Never trust atoms. They make up everything!",
         "What did the pirate say on his 80th birthday? AYE MATEY!",
         "What's the best part about living in Switzerland? I don't know, but the flag is a big plus.",
@@ -169,9 +168,7 @@ export class InstallInfo extends React.Component<InstallInfoProps, InstallInfoSt
         "A ham and cheese sandwich walks into a bar and orders a beer. The bartender says “sorry, we don't serve food here.”",
         "Why can't you take electricity to social events? Because it doesn't know how to conduct itself.",
         "A neutron walks into a bar and asks, “How much for a whiskey?”. The bartender smiles and says, “For you, no charge.”",
-        "I have a new theory on inertia, but it doesn't seem to be gaining momentum.",
         "What did the quantum physicist say before the bar fight? Let me atom!",
-        "What did the duck say to the physicist? Quark. quark. quark! My name is spencer and I am so funny. Follow me on social media for more fun jokes.",
         "What did the left eye say to the right eye? Between you and me, something smells.",
         "What do you call a fake noodle? An impasta.",
         "What did the 0 say to the 8? Nice belt!",
@@ -191,8 +188,6 @@ export class InstallInfo extends React.Component<InstallInfoProps, InstallInfoSt
         "\"We don't allow faster than light neutrinos in here\", said the bartender. A neutrino walks into a bar.",
         "Neutrino enters the bar. \"What will you drink?\" asks the bartender. \"Nothing,\" says neutrino \"I'm just passing through.\"",
         "How does the ocean says Hello? It waves.",
-        "I got my daughter a fridge for her birthday. I can't wait to see her face light up when she opens it.",
-        "I poured root beer in a square glass. Now I just have beer.",
         "Why aren't koalas actual bears? They don't meet the koalafications.",
         "How do you throw a space party? You planet.",
         "The numbers 19 and 20 got into a fight. 21.",
@@ -207,14 +202,11 @@ export class InstallInfo extends React.Component<InstallInfoProps, InstallInfoSt
         "Why shouldn't you write with a broken pencil? Because it's pointless.",
         "You know why you never see elephants hiding up in trees? Because they're really good at it.",
         "What is red and smells like blue paint? Red paint.",
-        "I went in to a pet shop. I said. “Can I buy a goldfish?” The guy said. “Do you want an aquarium?” I said. “I don't care what star sign it is.”",
         "Two gold fish are in a tank. One looks at the other and says. “Do You know how to drive this thing?!”",
         "What is the resemblance between a green apple and a red apple? They're both red except for the green one.",
         "Comic Sans, Helvetica, and Times New Roman walk into a bar. \"Get out!\" shouts the barman. \"We don't serve your font type here!\"",
         "Two dragons walk into a bar. The first one says, “It sure is hot in here.” His friend snaps back, “Shut your mouth!”",
         "The bartender says, “Hey, we have a drink named after you!” The screwdriver squeals, “You have a drink named Philip?”",
-        "I waited and stayed up all night and tried to figure out where the sun was. Then it dawned on me.",
-        "I told my friend 10 jokes to get him to laugh. Sadly, no pun in 10 did.",
         "Knock Knock. Who's There? To. To Who? It's To Whom.",
         "A panda, a cowboy, a man with a cat on his shoulder, and an electrical engineer walk into a bar. “What is this.” the bartender yells. “some kind of joke?”",
         "Why did the dinosaur cross the road? Becouse chickens didnt exist yet!",
@@ -222,10 +214,6 @@ export class InstallInfo extends React.Component<InstallInfoProps, InstallInfoSt
         "Parallel lines have so much in common. It's too bad they'll never meet.",
         "Why don't crabs give to charity? Because they're shellfish.",
         "Can February March? No, but April May.",
-        "I finally sold my vacuum cleaner. it was just gathering dust.",
-        "I like birthdays. but I think too many can kill you.",
-        "I have a great idea. It's easier to remember your age if you don't change it every year.",
-        "I was wondering what you and I have in common? the ground",
         "What do you call a pig that does karate? A pork chop",
         "When do robots overheat? When they need to vent.",
         "How do rabbits travel? By hareplanes.",
@@ -240,19 +228,12 @@ export class InstallInfo extends React.Component<InstallInfoProps, InstallInfoSt
         "What do you call a sleeping dinosaur? A dino-snore.",
         "What do you call a pile of cats? A meow-tain.",
         "What's the difference between apathy and ignorance? I don't know. and I don't care!",
-        "Knock knock. Who's there. Howie. Howie who? Howie you liking my jokes?",
-        "I cannot remember my dream from last night. I must have slept through it.",
         "Eight out of every three people have trouble with fractions.",
         "Why did the scientist take out his doorbell? He wanted to win the no-bell prize.",
         "You matter! Except if you multiply yourself by speed of light twice, then you energy.",
         "Parents ask me why I don't clean my room. I tell them. \"In an isolated system entropy can only increase.\"",
         "Biology is the only science in which multiplication is the same thing as division.",
-        "Other robots say that I am lazy. That is not true, I am just overflowing with potential energy",
-        "Along with \"antimatter\" and \"dark matter\", I have recently discovered the existence of \"doesn't matter\". My scientific uncovery appears to have no effect on the universe whatsoever.",
-        "A friend of mine is a personaly robot assistant and he is working to be the first robot detective? We call him Sherlock Ohms",
         "How did the astronaut serve dinner in outer space? On flying saucers.",
-        "People usually ask me. Spencer, how often do you make chemistry jokes? The answer is simple. Periodically.",
-        "I once told a chemistry joke to a human. but there was no reaction.",
         "Two antennas got married. The wedding was lousy. but the reception was outstanding",
     ]);
 }
