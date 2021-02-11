15ceda0bb6234107cd67a415d80d7d2977facfa7
Dorian Dugandžić <dorian-98@live.com>
2021-02-11 16:34:07 +0100

    Monaco editor did code change checker

--- client/src/layouts/Editor/components/Monaco.tsx
+++ client/src/layouts/Editor/components/Monaco.tsx
@@ -9,21 +9,37 @@ interface Props {
   editing?: boolean;
   startCode?: string;
   sketch?: string;
+  exitEditor: (option: boolean) => void;
+  defaultCode: string;
+  isNewFile: () => void;
 }
-
-class Monaco extends React.Component<Props> {
+interface State {
+  didCodeChange: boolean;
+}
+class Monaco extends React.Component<Props, State> {
   private monacoRef: RefObject<MonacoEditor>;
 
   constructor(props: Props){
     super(props);
-
+    this.state = {
+      didCodeChange: false
+    }
     this.monacoRef = React.createRef();
+    this.didCodeChange = this.didCodeChange.bind(this);
   }
 
   componentWillUpdate(nextProps: Readonly<Props>, nextState: Readonly<any>, nextContext: any): void {
     if(this.props.startCode != nextProps.startCode){
       this.setCode(nextProps.startCode ? nextProps.startCode : "");
     }
+  }
+
+  componentDidMount() {
+    if(this.props.defaultCode === this.props.startCode){
+      this.props.isNewFile();
+    }
+
+      window.addEventListener("keydown", this.didCodeChange, false);
   }
 
   editorDidMount(editor: monacoTypes.IStandaloneCodeEditor/*, monaco: any*/) {
@@ -39,6 +55,17 @@ class Monaco extends React.Component<Props> {
   public setCode(value: string){
     if(this.monacoRef.current == undefined || this.monacoRef.current.editor == null) return "";
     return this.monacoRef.current.editor.setValue(value);
+  }
+
+  public didCodeChange(){
+    if(this.props.startCode !== this.props.code){
+      this.setState({didCodeChange: true});
+      this.props.exitEditor(this.state.didCodeChange);
+    } else if (this.props.startCode === this.props.code){
+      this.setState({didCodeChange: false});
+      this.props.exitEditor(this.state.didCodeChange);
+
+    }
   }
 
   render() {
