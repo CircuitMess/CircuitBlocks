7c492612e6a261c5b774cc3d10b6e70e7e027224
Filip Budiša <filip@circuitmess.com>
2021-04-26 17:06:20 +0200

    Add JayD firmware restore

--- client/src/layouts/Home/components/RestoreFirmware.tsx
+++ client/src/layouts/Home/components/RestoreFirmware.tsx
@@ -36,6 +36,7 @@ export class RestoreFirmware extends React.Component<RestoreFirmwareProps, Resto
                         <Button primary onClick={() => callback("cm:esp32:ringo")} style={{margin: "0 15px"}}>Ringo</Button>
                         <Button primary onClick={() => callback("cm:esp8266:nibble")} style={{margin: "0 15px"}}>Nibble</Button>
                         <Button primary onClick={() => callback("cm:esp32:spencer")} style={{margin: "0 15px"}}>Spencer</Button>
+                        <Button primary onClick={() => callback("cm:esp32:jayd")} style={{margin: "0 15px"}}>JayD</Button>
                     </div>
                 </div>
             </ModalBase>
