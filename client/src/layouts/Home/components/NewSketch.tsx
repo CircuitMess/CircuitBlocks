028306dd99bf80e518c92ac53ddb3027589fa315
Filip Budiša <filip@circuitmess.com>
2021-05-12 13:23:05 +0200

    Fix NewSketch modal wrapping

--- client/src/layouts/Home/components/NewSketch.tsx
+++ client/src/layouts/Home/components/NewSketch.tsx
@@ -39,11 +39,11 @@ export class NewSketch extends React.Component<NewSketchProps, NewSketchState> {
                 <div className="title" style={{ position: "relative", fontSize: 26, top: 0, textAlign: "center", marginBottom: 30, lineHeight: 1.2 }}>New sketch</div>
                 <div className="content" style={{ display: "flex", flexDirection: "column", justifyContent: "space-between" }}>
                     <p style={{ marginBottom: 15, fontSize: 18, fontWeight: "bold" }}>Device:</p>
-                    <div style={{ display: "flex", flexDirection: "row", justifyContent: "center", marginBottom: 20 }}>
-                        <Button primary inverted={this.state.device !== "cm:esp32:ringo"} onClick={() => this.setState({ device: "cm:esp32:ringo" }) } style={{margin: "0 15px", height:40}}>Ringo</Button>
-                        <Button primary inverted={this.state.device !== "cm:esp8266:nibble"} onClick={() => this.setState({ device: "cm:esp8266:nibble" }) } style={{margin: "0 15px", height:40}}>Nibble</Button>
-                        <Button primary inverted={this.state.device !== "cm:esp32:spencer"} onClick={() => this.setState({  device: "cm:esp32:spencer" }) } style={{margin: "0 15px", height:40}}>Spencer</Button>
-                        <Button primary inverted={this.state.device !== "cm:esp32:jayd"} onClick={() => this.setState({  device: "cm:esp32:jayd" }) } style={{margin: "0 15px", height:40}}>Jay-D</Button>
+                    <div style={{ display: "flex", flexDirection: "row", justifyContent: "center", marginBottom: 20, flexWrap: "wrap" }}>
+                        <Button primary inverted={this.state.device !== "cm:esp32:ringo"} onClick={() => this.setState({ device: "cm:esp32:ringo" }) } style={{margin: "0 15px", height:40, whiteSpace: "nowrap", marginBottom: 10 }}>Ringo</Button>
+                        <Button primary inverted={this.state.device !== "cm:esp8266:nibble"} onClick={() => this.setState({ device: "cm:esp8266:nibble" }) } style={{margin: "0 15px", height:40, whiteSpace: "nowrap", marginBottom: 10 }}>Nibble</Button>
+                        <Button primary inverted={this.state.device !== "cm:esp32:spencer"} onClick={() => this.setState({  device: "cm:esp32:spencer" }) } style={{margin: "0 15px", height:40, whiteSpace: "nowrap", marginBottom: 10 }}>Spencer</Button>
+                        <Button primary inverted={this.state.device !== "cm:esp32:jayd"} onClick={() => this.setState({  device: "cm:esp32:jayd" }) } style={{margin: "0 15px", height:40, whiteSpace: "nowrap", marginBottom: 10 }}>Jay-D</Button>
 
                     </div>
                     <p style={{ marginBottom: 15, fontSize: 18, fontWeight: "bold" }}>Sketch type:</p>
