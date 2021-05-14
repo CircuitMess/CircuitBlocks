aff517d2c4cbfd783f05976842cde714f0192500
Filip Budiša <filip@circuitmess.com>
2021-05-14 14:50:36 +0200

    Bump version number

--- client/src/layouts/Home/index.tsx
+++ client/src/layouts/Home/index.tsx
@@ -223,7 +223,7 @@ export default class Home extends React.Component<HomeProps, HomeState> {
                       /> )}
                 </Main>
                 <Footer>
-                  <p>v1.3.1 <span style={{ padding: "0 10px" }}>|</span> <a style={{ cursor: "pointer" }} onClick={ () => this.openErrorReport() }>Send error report</a></p>
+                  <p>v1.4.0 <span style={{ padding: "0 10px" }}>|</span> <a style={{ cursor: "pointer" }} onClick={ () => this.openErrorReport() }>Send error report</a></p>
                 </Footer>
               </>
           ) : (
